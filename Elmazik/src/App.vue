1050f9c0aecb550b578d9945c17bb667f03e584d
Elmazzik <mustafaeva.e.pi.21@gmail.com>
2023-02-11 21:12:56 +0300

    Реактивное условие в Vue

--- Elmazik/src/App.vue
+++ Elmazik/src/App.vue
@@ -4,12 +4,22 @@ import { normalizeProps } from 'vue';
 export default {
   data() {
     return {
-      hidden: true,
+      visible: true,
     }
 },
+  methods: {
+    hide: function () {
+      this.visible = false;
+    },
+    nothide: function () {
+      this.visible = true;
+    }
+  }
 };
 </script>
 
 <template>
-  <p v-if="!hidden">Lorem ipsum</p>
+  <button @click="hide" v-if="visible">hide</button>
+  <button @click="nothide" v-if="!visible">not hide</button>
+  <p v-if="visible">Hello!</p>
 </template>
