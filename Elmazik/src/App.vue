cbd178b05785641df1792e55ededdb03a3d14b35
Elmazzik <mustafaeva.e.pi.21@gmail.com>
2023-02-12 14:54:51 +0300

    Тогглинг элементов в Vue

--- Elmazik/src/App.vue
+++ Elmazik/src/App.vue
@@ -4,18 +4,22 @@ import { normalizeProps } from 'vue';
 export default {
   data() {
     return {
-      visible: true,
+      visible: [ true, true, true]
     }
 },
   methods: {
-    toggle: function () {
-      this.visible = !this.visible;
+    toggle: function (i) {
+      this.visible[i] = !this.visible[i];
     }
   }
 };
 </script>
 
 <template>
-  <button @click="toggle">first</button>
-  <p v-if="visible">one</p>
+  <button @click="toggle(1)">first</button>
+  <p v-if="visible[1]">one</p>
+  <button @click="toggle(2)">second</button>
+  <p v-if="visible[2]">two</p>
+  <button @click="toggle(3)">third</button>
+  <p v-if="visible[3]">three</p>
 </template>
