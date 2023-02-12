07d202b9cbb7de098e8cf32089b1488b7e47317b
Elmazzik <mustafaeva.e.pi.21@gmail.com>
2023-02-12 15:06:37 +0300

    Директива v-else в Vue

--- Elmazik/src/App.vue
+++ Elmazik/src/App.vue
@@ -4,22 +4,13 @@ import { normalizeProps } from 'vue';
 export default {
   data() {
     return {
-      visible: [ true, true, true]
-    }
-},
-  methods: {
-    toggle: function (i) {
-      this.visible[i] = !this.visible[i];
+      isAdmin: true,
     }
   }
 };
 </script>
 
 <template>
-  <button @click="toggle(1)">{{ visible[1] ? 'hide' : 'show' }}</button>
-  <p v-if="visible[1]">one</p>
-  <button @click="toggle(2)">{{ visible[2] ? 'hide' : 'show' }}</button>
-  <p v-if="visible[2]">two</p>
-  <button @click="toggle(3)">{{ visible[3] ? 'hide' : 'show' }}</button>
-  <p v-if="visible[3]">three</p>
+  <p v-if="isAdmin">+++</p>
+  <p v-else>---</p>
 </template>
