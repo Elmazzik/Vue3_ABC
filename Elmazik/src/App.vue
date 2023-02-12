57910463ec778383f52b19a251532470cab9a64d
Elmazzik <mustafaeva.e.pi.21@gmail.com>
2023-02-12 15:11:40 +0300

    Сложные условия в Vue

--- Elmazik/src/App.vue
+++ Elmazik/src/App.vue
@@ -4,13 +4,18 @@ import { normalizeProps } from 'vue';
 export default {
   data() {
     return {
-      isAdmin: true,
+      num: 5,
     }
   }
 };
 </script>
 
 <template>
-  <p v-if="isAdmin">+++</p>
-  <p v-else>---</p>
+  <p v-if="num === 1">monday</p>
+  <p v-if="num === 2">tuesday</p>
+  <p v-if="num === 3">wednesday</p>
+  <p v-if="num === 4">thursday</p>
+  <p v-if="num === 5">friday</p>
+  <p v-if="num === 6">saturday</p>
+  <p v-if="num === 7">sunday</p>
 </template>
