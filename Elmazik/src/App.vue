7ae0bbdd6a5600085a820d7821e449397af100a7
Elmazzik <mustafaeva.e.pi.21@gmail.com>
2023-02-12 15:00:40 +0300

    Смена текста при тогглинге в Vue

--- Elmazik/src/App.vue
+++ Elmazik/src/App.vue
@@ -4,18 +4,22 @@ import { normalizeProps } from 'vue';
 export default {
   data() {
     return {
-      visible: true,
+      visible: [ true, true, true]
     }
 },
   methods: {
-    toggle: function () {
-      this.visible = !this.visible;
+    toggle: function (i) {
+      this.visible[i] = !this.visible[i];
     }
   }
 };
 </script>
 
 <template>
-  <button @click="toggle">first</button>
-  <p v-if="visible">one</p>
+  <button @click="toggle(1)">{{ visible[1] ? 'hide' : 'show' }}</button>
+  <p v-if="visible[1]">one</p>
+  <button @click="toggle(2)">{{ visible[2] ? 'hide' : 'show' }}</button>
+  <p v-if="visible[2]">two</p>
+  <button @click="toggle(3)">{{ visible[3] ? 'hide' : 'show' }}</button>
+  <p v-if="visible[3]">three</p>
 </template>
