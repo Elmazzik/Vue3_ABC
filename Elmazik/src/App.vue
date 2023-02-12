dd9a9550888c3d2ed4ea8fc726b420f658eb7c19
Elmazzik <mustafaeva.e.pi.21@gmail.com>
2023-02-12 18:40:53 +0300

    Условия в циклах в Vue

--- Elmazik/src/App.vue
+++ Elmazik/src/App.vue
@@ -4,13 +4,18 @@ import { normalizeProps } from 'vue';
 export default {
   data() {
     return {
+      items: [1, -2, 3, -4, 5],
     }
   }
 }
 </script>
 
 <template>
-    <p v-for="num in 30">
-      {{ num }}
-    </p>
+<ul>
+  <template v-for="elem in items">
+    <li v-if="elem > 0">
+      {{ elem }}
+    </li>
+  </template>
+</ul>
 </template>
