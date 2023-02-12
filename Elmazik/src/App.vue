c1be0d917f1552e1f175a21065a98333816e6497
Elmazzik <mustafaeva.e.pi.21@gmail.com>
2023-02-12 15:57:00 +0300

    Перебор объектов в Vue

--- Elmazik/src/App.vue
+++ Elmazik/src/App.vue
@@ -4,14 +4,32 @@ import { normalizeProps } from 'vue';
 export default {
   data() {
     return {
-      arr: ['x', 'y', 'z'],
+      obj: {
+        user1: '100$',
+        user2: '200$',
+        user3: '300$',
+      },
     }
   },
 }
 </script>
 
 <template>
+Задание №1
     <ul>
-      <li v-for="(elem, key) in arr">{{ key + 1 }} {{ elem }}</li>
+      <li v-for="elem in obj">
+		{{ elem }}</li>
+    </ul>
+Задание №2
+    <ul>
+      <li v-for="(elem, key) in obj">
+		{{ key }} {{ elem }}</li>
+    </ul>
+Задание №3
+    <ul>
+      <li v-for="(elem, key, index) in obj">
+		{{ index }}
+		{{ key }}
+		{{ elem }}</li>
     </ul>
 </template>
