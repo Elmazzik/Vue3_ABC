8a78d8e16ef6126afd13e6bad21c1b9917ef897a
Elmazzik <mustafaeva.e.pi.21@gmail.com>
2023-02-12 18:38:19 +0300

    Цикл в диапазоне в Vue

--- Elmazik/src/App.vue
+++ Elmazik/src/App.vue
@@ -4,32 +4,13 @@ import { normalizeProps } from 'vue';
 export default {
   data() {
     return {
-      products: [
-        {
-          name: 'product1',
-          price: 100,
-          quantity: 5
-        },
-        {
-          name: 'product2',
-          price: 200,
-          quantity: 4
-        },
-        {
-          name: 'product3',
-          price: 300,
-          quantity: 3
-        },
-      ]
     }
   }
 }
 </script>
 
 <template>
-    <table v-for="product in products">
-    {{ product.name }}
-    {{ product.price }}
-    {{ product.quantity }}
-    </table>
+    <p v-for="num in 30">
+      {{ num }}
+    </p>
 </template>
