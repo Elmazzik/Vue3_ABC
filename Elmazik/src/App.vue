0883e279437e5cd75bf4327ba245f732c0b86119
Elmazzik <mustafaeva.e.pi.21@gmail.com>
2023-02-12 15:42:47 +0300

    Перебор массивов в Vue

--- Elmazik/src/App.vue
+++ Elmazik/src/App.vue
@@ -4,18 +4,16 @@ import { normalizeProps } from 'vue';
 export default {
   data() {
     return {
-      isAuth: true,
-}
-},
-methods: {
-    toggle: function () {
-    this.isAuth = !this.isAuth;
+      items: [1, 2, 3, 4, 5],
     }
-  }
-};
+  },
+}
 </script>
 
 <template>
-  <p v-show="isAuth">+++</p>
-  <button @click="toggle">скрыть</button>
+  <p v-for="elem in items">{{ elem }}</p>
+    <div v-for="elem in items">{{ elem ** 2 }}</div>
+    <ul>
+      <li v-for="elem in items">{{ elem }}</li>
+    </ul>
 </template>
