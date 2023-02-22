54e7b95459a76e102bfd8fe68b83e4d3301054f2
Elmazzik <mustafaeva.e.pi.21@gmail.com>
2023-02-22 22:15:29 +0300

    Работа с селектами в Vue

--- Elmazik/src/App.vue
+++ Elmazik/src/App.vue
@@ -2,39 +2,21 @@
 export default {
   data() {
     return {
-      language: '',
-      visible: false,
-      visible1: false,
-      visible2: false,
+      selected: 'Симферополь',
     }
   },
   methods: {
-    one() {
-      this.visible = true;
-    },
-    two() {
-      this.visible1 = true;
-    },
-    three() {
-      this.visible2 = true;
-    }
   }
 }
 </script>
 <template>
-      <input type="radio" v-model="language" value="Русский" @click="first">
-      <p>Turkish</p>
-      <p></p>
-      <input type="radio" v-model="language" value="Английский" @click="second">
-      <p>Ukrainian</p>
-      <p></p>
-      <input type="radio" v-model="language" value="Крымскотатарский" @click="third">
-      <p></p>
-      <p>Russian</p>
-      <p v-if="visible">Привет, будешь чай?</p>
-      <p v-else-if="visible1">Hi, do you want some tea?</p>
-      <p v-else-if="visible2">Селям, чай ичеджен? </p>
-
+      <p>В каком городе ты живешь?</p>
+      <select v-model="selected">
+        <option>Симферополь</option>
+        <option>Нью-Йорк</option>
+        <option>Сыктывкар</option>
+      </select>
+      <p>{{ selected }}</p>
 </template>
 <style>
 
