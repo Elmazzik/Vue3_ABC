7e584a5f07c60fef626c7eac43de53eae54fac24
Elmazzik <mustafaeva.e.pi.21@gmail.com>
2023-02-22 22:10:18 +0300

    Работа с радиокнопками в Vue

--- Elmazik/src/App.vue
+++ Elmazik/src/App.vue
@@ -2,23 +2,39 @@
 export default {
   data() {
     return {
-      arr: [],
+      language: '',
+      visible: false,
+      visible1: false,
+      visible2: false,
+    }
+  },
+  methods: {
+    one() {
+      this.visible = true;
+    },
+    two() {
+      this.visible1 = true;
+    },
+    three() {
+      this.visible2 = true;
     }
   }
 }
 </script>
 <template>
-  <input type="checkbox" v-model="arr" value="Русский">
-  <label>Русский</label>
-  <br>
-  <input type="checkbox" v-model="arr" value="Английский">
-  <label>Английский</label>
-  <br>
-  <input type="checkbox" v-model="arr" value="Крымскотатарский">
-  <label>Крымскотатарский</label>
-  <ul>
-  <li v-for="elem in arr">{{ elem }}</li>
-  </ul>
+      <input type="radio" v-model="language" value="Русский" @click="first">
+      <p>Turkish</p>
+      <p></p>
+      <input type="radio" v-model="language" value="Английский" @click="second">
+      <p>Ukrainian</p>
+      <p></p>
+      <input type="radio" v-model="language" value="Крымскотатарский" @click="third">
+      <p></p>
+      <p>Russian</p>
+      <p v-if="visible">Привет, будешь чай?</p>
+      <p v-else-if="visible1">Hi, do you want some tea?</p>
+      <p v-else-if="visible2">Селям, чай ичеджен? </p>
+
 </template>
 <style>
 
