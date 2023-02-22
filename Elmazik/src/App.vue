418bb89d1adec5faf53ba17786977974ab577000
Elmazzik <mustafaeva.e.pi.21@gmail.com>
2023-02-22 21:07:36 +0300

    Двусторонняя привязка данных к инпутам в Vue

--- Elmazik/src/App.vue
+++ Elmazik/src/App.vue
@@ -2,13 +2,20 @@
 export default {
   data() {
     return {
+      message: 'Эльмз',
+      number: 'Мустафаева',
     }
+  },
+  methods: {
   }
 }
 </script>
 <template>
-  <p :style="{ color: 'green', background: 'yellow' }">Какой-то абзац</p>
-  <p :style="{ fontWeight: 'bold', fontStyle: 'italic' }">Ещё один абзац</p>
+  <input class="text-field__input" v-model="message">
+  <p :style="{ textTransform: 'uppercase' }">{{ message }}</p>
+  <br>
+  <input class="text-field__input" v-model="number">
+  <p>{{ number * number }}</p>
 </template>
 <style>
 
