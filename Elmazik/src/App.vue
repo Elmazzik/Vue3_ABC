5d1874c885b48fe0d00d904614dd75f30f33368b
Elmazzik <mustafaeva.e.pi.21@gmail.com>
2023-02-22 21:32:28 +0300

    Работа с чекбоксами в Vue

--- Elmazik/src/App.vue
+++ Elmazik/src/App.vue
@@ -2,24 +2,19 @@
 export default {
   data() {
     return {
-      text: ' ',
-      cat: ' ',
+      checked: true,
     }
   },
   methods: {
-    list: function () {
+    toggle: function () {
+      this.checked = false;
     }
   }
 }
 </script>
 <template>
-  <textarea class="text-field__input" v-model="text"></textarea>
-  <p>{{ text }}</p>
-  <br>
-  <button @click="Elmaz">тык!</button>
-  <ul>
-  <li v-for="Text in text">{{ Text }}</li>
-  </ul>
+      <input type="checkbox" v-model="checked">
+      <p v-if="checked">{{ checked ? 'текст' : 'скрыто' }}</p>
 </template>
 <style>
 
