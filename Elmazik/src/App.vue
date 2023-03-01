2f61d839eaf5a224dbb888c8fead014d375b100c
Elmazzik <mustafaeva.e.pi.21@gmail.com>
2023-03-01 09:08:26 +0300

    Создание компонентов в цикле в Vue

--- Elmazik/src/App.vue
+++ Elmazik/src/App.vue
@@ -1,22 +1,38 @@
-<script>
+</script>
 import Employee from './components/Employee.vue'
 export default {
   components: {
     Employee
-  },
+  }, 
   data() {
-    return {
-      name: '"Эльмаз"',
-      surn: 'Мустафаева',
-      age: 18,
-      value: true,
-    }
-  }
+	return {
+		users: [
+			{
+				id: 1,
+				name: 'name1',
+				salary: 100,
+				age: 30,
+			},
+			{
+				id: 2,
+				name: 'name2',
+				salary: 200,
+				age: 40,
+			},
+			{
+				id: 3,
+				name: 'name3',
+				salary: 300,
+				age: 50,
+			},
+		],
+	}
+}
 }
 </script>
 
 <template>
-  <Employee :name="name" :surn="surn" :age="age" :value="value" />
+  <Employee v-for="user in users" :name="user.name" :salary="user.salary" :key="user.id" :age="user.age" />
 </template>
 
 <style>
