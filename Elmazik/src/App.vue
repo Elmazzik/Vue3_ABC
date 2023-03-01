8d1b19973aa9f39f87a2ad051b7cf168f5ca7d31
Elmazzik <mustafaeva.e.pi.21@gmail.com>
2023-03-01 09:09:36 +0300

    Однонаправленный поток данных в Vue

--- Elmazik/src/App.vue
+++ Elmazik/src/App.vue
@@ -1,35 +1,36 @@
-</script>
+<script>
 import Employee from './components/Employee.vue'
 export default {
   components: {
     Employee
-  }, 
+  },
   data() {
-	return {
-		users: [
-			{
-				id: 1,
-				name: 'name1',
-				salary: 100,
-				age: 30,
-			},
-			{
-				id: 2,
-				name: 'name2',
-				salary: 200,
-				age: 40,
-			},
-			{
-				id: 3,
-				name: 'name3',
-				salary: 300,
-				age: 50,
-			},
-		],
-	}
-}
+    return {
+      users: [
+        {
+          id: 1,
+          name: 'name1',
+          salary: 100,
+          age: 30,
+        },
+        {
+          id: 2,
+          name: 'name2',
+          salary: 200,
+          age: 40,
+        },
+        {
+          id: 3,
+          name: 'name3',
+          salary: 300,
+          age: 50,
+        },
+      ],
+    }
+  }
 }
 </script>
+
 
 <template>
   <Employee v-for="user in users" :name="user.name" :salary="user.salary" :key="user.id" :age="user.age" />
