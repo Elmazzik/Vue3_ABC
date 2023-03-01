bd4ebc6e2d052f767e7b573b9a25d3b6bdb05b9d
Elmazzik <mustafaeva.e.pi.21@gmail.com>
2023-03-01 08:56:07 +0300

    Работа с компонентами в Vue

--- Elmazik/src/App.vue
+++ Elmazik/src/App.vue
@@ -1,62 +1,16 @@
+<script>
 export default {
   data() {
     return {
-      users: [
-        {
-          id: 1,
-          name: 'name1',
-          salary: 100,
-          age: 30,
-        },
-        {
-          id: 2,
-          name: 'name2',
-          salary: 200,
-          age: 40,
-        },
-        {
-          id: 3,
-          name: 'name3',
-          salary: 300,
-          age: 50,
-        },
-      ],
     }
   },
   methods: {
-    removeUser: function (id) {
-      this.users = this.users.filter((user) => {
-        return user.id !== id;
-      })
-    },
-    edit(user) {
-      user.isEdit = true;
-    },
-    save(user) {
-      user.isEdit = false;
-    },
   }
 }
 </script>
 
 <template>
-  <table class="tb">
-    <tr v-for="user in users" :key="user.id">
-      <template v-if="!user.isEdit">
-        <td class="block">{{ user.name }}</td>
-        <td class="block">{{ user.salary }}</td>
-        <td class="block">{{ user.age }}</td>
-        <td class="block"><button class="btn" @click="removeUser(user.id)">delete</button></td>
-        <td class="block"><button class="btn" @click="edit(user)">update</button></td>
-      </template>
-      <template v-else>
-        <td class="block"><input class="block_input" v-model="user.name"></td>
-        <td class="block"><input class="block_input" v-model="user.salary"></td>
-        <td class="block"><input class="block_input" v-model="user.age"></td>
-        <td class="block"><button class="btn" @click="save(user)">save</button></td>
-      </template>
-    </tr>
-  </table>
+
 </template>
 <style>
 
